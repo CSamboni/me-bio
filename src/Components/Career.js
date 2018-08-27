5273cb80ab1cc71db127a7d38217a8cb273ca58f
CSamboni <elementrixz512@gmail.com>
2018-08-26 19:58:51 -0500

    App funcional

---
+++ src/Components/Career.js
@@ -0,0 +1,143 @@
+import React, {Component} from 'react';
+
+class Career extends Component {
+    render() {
+        return (
+            <div>
+                <div className="column-is-mobile">
+                    <div className="title bio">EDUCATION
+                    </div>
+                </div>
+                {/*Card 1 (Education)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            EDUCATION
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Frontend Web Development.
+                                </strong>
+                                Códigofacilito (2015-2018)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 2 (Education)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            EDUCATION
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>JavaScript Development.
+                                </strong>
+                                Códigofacilito (2015-2018)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 3 (Education)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            EDUCATION
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Media & Animations.</strong>
+                                Platzi (2014-2017)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 4 (Education)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            EDUCATION
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Networking & Iot.</strong>
+                                Fututel SAS (2015-2018)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="title bio">PROFESSIONAL EXPERIENCE
+                </div>
+                {/*Card 5 (Professional Experience)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            PROFESSIONAL EXPERIENCE
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Technical support</strong>. Attencion, supporting & technical systems computer Maintance. (2012-Current)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 6 (Professional Experience)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            PROFESSIONAL EXPERIENCE
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Blogger & writer at Fututel SAS</strong>. Writting for post, blogs and
+                                publishing documentation and tutorials tech. (2015-2016)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 7 (Professional Experience)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            PROFESSIONAL EXPERIENCE
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Web Developer</strong>. Developing for web applications with React & Nodejs.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 8 (Certificates)*/}
+                <div className="title bio">CERTIFICATES | AWARDS</div>
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            CERTIFICATES | AWARDS
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>JavaScript course & design</strong>. Winner of Profesional JavaScript course & design UX. (2017)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Card 9 (Certificates)*/}
+                <div className="card margin-card">
+                    <div className="card-header">
+                        <p className="card-header-title">
+                            CERTIFICATES | AWARDS
+                        </p>
+                        <div className="card-content">
+                            <div className="content">
+                                <strong>Professional web Developer Course</strong>. Certified by Codigofacilito as a Web Developer.(2018)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Career;
---
+++ src/Components/Header.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import '../App.css';
+import {SocialIcon} from 'react-social-icons';
+
+const ImageUrl = 'https://avatars1.githubusercontent.com/u/24655632?s=460&v=4';
+class Header extends Component {
+    render() {
+        return (
+            <div className="column-is-mobile">
+                <div className="image is-128x128">
+                    <img className="is-rounded profile" src={ImageUrl} alt="Profile"/>
+                </div>
+                <div
+                    className="tile is-ancestor"
+                    style={{
+                    'margin': '10px'
+                }}>
+                    {/*Description*/}
+                    <div className="is-vertical is-8 ">
+                        <div className="tile">
+                            <div className="tile is-parent is-vertical">
+                                <div className="tile is-child notification is-primary">
+                                    <p className="title">Hi! My name is Cristian Samboni.</p>
+                                    <p className="subtitle">I'm a Frontend Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*Section Media*/}
+                <div className="row social">
+                    <SocialIcon
+                        style={{
+                        'margin': '10px'
+                    }}
+                        url="https://www.facebook.com/CrisstianSambonii"
+                        network="facebook"/>
+                    <SocialIcon
+                        style={{
+                        'margin': '10px'
+                    }}
+                        url="https://github.com/csamboni"
+                        network="github"/>
+                    <SocialIcon
+                        style={{
+                        'margin': '10px'
+                    }}
+                        url="https://twitter.com/Cris_SamJS"
+                        network="twitter"/>
+                    <SocialIcon
+                        style={{
+                        'margin': '10px'
+                    }}
+                        url="https://medium.com/@CristianSAM"
+                        network="medium"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
